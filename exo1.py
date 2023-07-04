a16e62eb9c59eadbad716c8e33f2e659f363ce26
Mayalale Clément Kafwimbi <90102062+Menclet@users.noreply.github.com>
2023-07-04 09:53:03 +0000

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,8 @@
+def AnneeBiss(n):
+    if (n%4==0 and n%100!=0) or n%400==0:
+        print("{} est une année bissextile".format(n))   
+    else:
+        print("{} n'est pas une année bissextile".format(n))
+
+    return None
+AnneeBiss(400)
